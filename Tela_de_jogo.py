e2903c63d3cdf64ab7c232b6a0a3790816e4b751
Nina <ninars@al.insper.edu.br>
2022-05-25 19:03:05 -0300

    commit nina

--- Tela_de_jogo.py
+++ Tela_de_jogo.py
@@ -5,7 +5,7 @@ from os import path
 def gameplay(janela):
     tempo_fps = pygame.time.Clock()
     imagem_raposa = pygame.image.load(path.join(DIR_IMG, 'imagem_humberto.png')).convert()
-    imagem_raposa = pygame.transform.scale(imagem_raposa, (600, 400))
+    imagem_raposa = pygame.transform.scale(imagem_raposa, (150, 200))
     rodando = True
     while rodando:
         tempo_fps.tick(FPS)

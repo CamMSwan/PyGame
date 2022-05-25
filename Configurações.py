3007da13c303e63890aa916899b478466346399a
CamMSwan <cameronms@al.insper.edu.br>
2022-05-25 17:54:21 -0300

    Commit Cameron

--- Configurações.py
+++ Configurações.py
@@ -1,19 +1,19 @@
 from os import path
 
-DIR_IMG = path.join(path.dirname(__file__), 'imagens', 'img')
-DIR_SOM = path.join(path.dirname(__file__), 'sons', 'snd')
-DIR_FONT = path.join(path.dirname(__file__), 'fontes', 'font')
+DIR_IMG = path.join(path.dirname(__file__), 'imagens')
+DIR_SOM = path.join(path.dirname(__file__), 'sons')
+DIR_FONT = path.join(path.dirname(__file__), 'fontes')
 
 LARGURA = 960
 ALTURA = 540 
 FPS = 60 
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+VERMELHO = (255, 0, 0)
+VERDE = (0, 255, 0)
+AZUL = (0, 0, 255)
+AMARELO = (255, 255, 0)
 
 INIT = 0
 GAME = 1

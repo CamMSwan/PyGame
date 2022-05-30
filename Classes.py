4912a4626fd022a81f59667411145192552e9424
mariana3albuquerque <91220409+mariana3albuquerque@users.noreply.github.com>
2022-05-30 16:58:07 -0300

    classe bala

--- Classes.py
+++ Classes.py
@@ -51,4 +51,22 @@ class player2(pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
 
-  
+class Bala(pygame.sprite.Sprite):
+    def __init__(self, img ,bottom,centerx):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img 
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+
+        #definindo lugar em x e y
+
+        self.rect.centerx = centerx
+        self.rect.bottom = bottom
+        self.speedx = 15
+
+    def update(self):
+        self.rect.x += self.speedx
+        # se a sala passar do fim da tela, desaparece
+        if self.rect.centerx > 960:
+            self.kill()

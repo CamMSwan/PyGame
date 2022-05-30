b9f3c1cb9d871800833428fb08d575c23e28b57a
Nina <ninars@al.insper.edu.br>
2022-05-30 16:08:23 -0300

    Commit Nina

--- teste.py
+++ teste.py
@@ -3,20 +3,29 @@ import pygame
 import random
 
 from sqlalchemy import false
+from sympy import fps
+
+from Elementos import FOX_IMG
 
 #Andar jogador 1
 
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        game = false
-    if event.type==pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            player.speedx += 8
-        if event.key == pygame.K_RIGHT:
-            player.speedx += 8
-    
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT:
-            player.speedx += 8
-        if event.key == pygame.K_RIGHT:
-            player.speedx += 8
+game = True
+clock = pygame.time.Clock()
+FPS = 60
+
+while game:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = false
+        if event.type==pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                FOX_IMG.speedx += 8
+            if event.key == pygame.K_RIGHT:
+                FOX_IMG.speedx += 8
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                FOX_IMG.speedx += 8
+            if event.key == pygame.K_RIGHT:
+                FOX_IMG.speedx += 8

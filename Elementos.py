205c7ac81bd94ea82529be54552f266941a3169a
CamMSwan <cameronms@al.insper.edu.br>
2022-05-30 22:11:35 -0300

    commit cameron

--- Elementos.py
+++ Elementos.py
@@ -13,10 +13,19 @@ ALTURA_DR = 100
 PLANO_DE_FUNDO = 'fundo2.png'
 MENU = 'fundo1.png'
 
+MACHADO = 'Animação machado'
+machado_anim = []
+
 def carregar_elementos():
     elementos = {}
     elementos[FOX_IMG] = pygame.image.load(os.path.join(DIR_IMG, 'imagem_humberto.png')).convert_alpha()
     elementos[FOX_IMG] = pygame.transform.scale(elementos['imagem_humberto.png'], (LARGURA_FOX, ALTURA_FOX))
     elementos[DOUTOR_IMG] = pygame.image.load(os.path.join(DIR_IMG, 'imagem_resina.png')).convert_alpha()
     elementos[DOUTOR_IMG] = pygame.transform.scale(elementos['imagem_resina.png'], (LARGURA_DR, ALTURA_DR))
+    for i in range(4):
+        arquivo = os.path.join(DIR_IMG,MACHADO, 'axe-1{}.png'.format(i))
+        img = pygame.image.load(arquivo).convert()
+        img = pygame.transform.scale(img, (32, 32))
+        machado_anim.append(img)
+    elementos[MACHADO] = machado_anim
     return elementos

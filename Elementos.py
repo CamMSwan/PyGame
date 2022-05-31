2b125d9dd716860ea32d48f55945c2c73926025a
CamMSwan <cameronms@al.insper.edu.br>
2022-05-31 16:14:58 -0300

    commit cameron

--- Elementos.py
+++ Elementos.py
@@ -2,7 +2,7 @@ import pygame
 import os
 from Configurações import DIR_IMG, DIR_SOM, DIR_FONT, LARGURA
 
-FOX_IMG = 'imagem2.png'
+FOX_IMG = 'raposa.png'
 LARGURA_FOX = 150
 ALTURA_FOX = 165
 
@@ -18,8 +18,8 @@ machado_anim = []
 
 def carregar_elementos():
     elementos = {}
-    elementos[FOX_IMG] = pygame.image.load(os.path.join(DIR_IMG, 'imagem2.png')).convert_alpha()
-    elementos[FOX_IMG] = pygame.transform.scale(elementos['imagem2.png'], (LARGURA_FOX, ALTURA_FOX))
+    elementos[FOX_IMG] = pygame.image.load(os.path.join(DIR_IMG, 'raposa.png')).convert_alpha()
+    elementos[FOX_IMG] = pygame.transform.scale(elementos['raposa.png'], (LARGURA_FOX, ALTURA_FOX))
     elementos[DOUTOR_IMG] = pygame.image.load(os.path.join(DIR_IMG, 'imagem_resina.png')).convert_alpha()
     elementos[DOUTOR_IMG] = pygame.transform.scale(elementos['imagem_resina.png'], (LARGURA_DR, ALTURA_DR))
     '''for i in range(4):
